f821a78b69ed8cc332171729edd3bc9c46a25c09
tyler-cranmer <tyler.cranmer@outlook.com>
2022-03-30 10:57:24 -0700

    Exceptioon.sol

---
+++ contracts/Exception.sol
@@ -0,0 +1,20 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.6.12;
+
+contract ExceptionExample {
+    mapping(address => uint64) public balanceReceived;
+    
+    function recieveMoney() public payable {
+        assert(msg.value == uint64(msg.value));
+        balanceReceived[msg.sender] += uint64(msg.value);
+        assert(balanceReceived[msg.sender] >= uint64(msg.value));
+    }
+    
+    
+    function withdrawMoney(address payable _to, uint64 _amount) public {
+        require(_amount <= balanceReceived[msg.sender], "not enough money in account");
+        assert(balanceReceived[msg.sender] <= balanceReceived[msg.sender] - _amount);
+            balanceReceived[msg.sender] -= _amount;
+            _to.transfer(_amount);
+    }
+}
